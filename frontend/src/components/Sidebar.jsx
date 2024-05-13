7818a8e69b29581b4038920829da0d98a806f00b
filippocaste <filippo.castellarin@studenti.polito.it>
2024-05-13 11:26:18 +0200

    add training options

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -8,6 +8,7 @@ import PlayFill from 'react-bootstrap-icons/dist/icons/play-fill'
 import ListColumns from 'react-bootstrap-icons/dist/icons/list-columns'
 
 import Blocks from '../../public/data/blocks.json'
+import { Dropdown, DropdownButton, Form } from 'react-bootstrap';
 
 
 const Sidebar = (props) => {
@@ -46,7 +47,7 @@ const Sidebar = (props) => {
                     </li>
                 </ul>
             </div>
-            {openMenu !== 'none' && <Menu setOpenMenu={setOpenMenu} openMenu={openMenu} nodes={nodes} />}
+            {openMenu !== 'none' && <Menu openMenu={openMenu} nodes={nodes} />}
         </>
     );
 };
@@ -87,8 +88,6 @@ const NetworkDesign = () => {
 
 const BlockTable = ({category, blocks}) => {
 
-    console.log(blocks)
-
     return (
         <>
             <h5 style={{textAlign: 'center'}}>{category}</h5>
@@ -120,14 +119,85 @@ const NetworkDetails = (props) => {
     );
 }
 
-const handleTrain = () => {
-    console.log("You successfully trained your network")
+const handleTrain = (params) => {
+    console.log("You successfully trained your network with this parameters: ", params)
 }
 
 const Training = () => {
+
+    // training parameters
+    const [learningRate, setLearningRate] = useState(0);
+    const [epochs, setEpochs] = useState(0);
+    const [batchSize, setBatchSize] = useState(0);
+    const [loss, setLoss] = useState('');
+    const [optimizer, setOptimizer] = useState('');
+    // --------------------------------------------------
+
     return (
         <>
-            <Button className='left-menu-button' onClick={() => handleTrain()}> Train </Button>
+            <Form className='mt-3'>
+                <Table striped variant={'dark'}>
+                    <tbody>
+                        <tr>
+                            <td style={{ textAlign: 'left' }}>Epoch</td>
+                            <td style={{ textAlign: 'right' }}>
+                                <Form.Group controlId="trainForm.Epoch" style={{ border: 'none' }} onChange={ev => setEpochs(ev.target.value)}>
+                                    <Form.Control type='number' style={{ width: '50%', float: 'right' }} />
+                                </Form.Group>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={{ textAlign: 'left' }}>Learning Rate</td>
+                            <td style={{ textAlign: 'right' }}>
+                                <Form.Group controlId="trainForm.Loss" style={{ border: 'none' }} onChange={ev => setLearningRate(ev.target.value)} >
+                                    <Form.Control type='number' style={{ width: '50%', float: 'right' }} />
+                                </Form.Group>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={{ textAlign: 'left' }}>Batch Size</td>
+                            <td style={{ textAlign: 'right' }}>
+                                <Form.Group controlId="trainForm.BatchSize" style={{ border: 'none' }} onChange={ev => setBatchSize(ev.target.value)} >
+                                    <Form.Control type='number' style={{ width: '50%', float: 'right' }} />
+                                </Form.Group>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td style={{ textAlign: 'left' }}>Loss Function</td>
+                            <td style={{ textAlign: 'right' }}>
+                                <DropdownButton
+                                    variant="outline-secondary"
+                                    title={loss || "Select"}
+                                    id="trainForm.LossFunction"
+                                    onSelect={sel => setLoss(sel)}
+                                >
+                                    <Dropdown.Item eventKey="CE">CE</Dropdown.Item>
+                                    <Dropdown.Item eventKey="SME">SME</Dropdown.Item>
+                                </DropdownButton>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td style={{ textAlign: 'left' }}>Optimizer</td>
+                            <td style={{ textAlign: 'right' }}>
+                                    <DropdownButton
+                                        variant="outline-secondary"
+                                        title={optimizer || "Select"}
+                                        id="trainForm.Optimizer"
+                                        onSelect={sel => {setOptimizer(sel)}}
+                                    >
+                                        <Dropdown.Item eventKey="SGD">SGD</Dropdown.Item>
+                                        <Dropdown.Item eventKey="Adam">Adam</Dropdown.Item>
+                                    </DropdownButton>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </Table>
+            </Form >
+
+            <Button className='left-menu-button' onClick={() => handleTrain({ epochs, learningRate, batchSize, loss, optimizer })}> Train </Button>
         </>
     );
 }
@@ -146,7 +216,7 @@ const handleExport = () => {
 const Options = () => {
     return (
         <>
-            <Button className='left-menu-button' onClick={() => handleSave()}> Save </Button> <br />
+            <Button className='left-menu-button mt-3' onClick={() => handleSave()}> Save </Button> <br />
             <Button className='left-menu-button' onClick={() => handleLoad()}> Load </Button> <br />
             <Button className='left-menu-button' onClick={() => handleExport()}> Export </Button>
         </>

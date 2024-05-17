f91f48f0f38be4defe2a158ba2fe97ce0f38d428
filippocaste <filippo.castellarin@studenti.polito.it>
2024-05-17 10:45:14 +0200

    style improvements

--- frontend/src/components/ResponseMessage.jsx
+++ frontend/src/components/ResponseMessage.jsx
@@ -2,21 +2,28 @@
 
 import Alert from "react-bootstrap/Alert";
 import { Check2, XCircleFill, ExclamationCircleFill, InfoCircleFill } from "react-bootstrap-icons";
+import { useEffect } from "react";
 
 export default function ResponseMessage(props) {
     const message = props.message;
     const variant = props.variant;
     const setShowMessage = props.setShowMesssage;
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowMessage(false);
+        }, 7000);
+
+        return () => clearTimeout(timer);
+    }, []);
     
     return (
-        <div className="response-message-container">
-            <Alert variant={variant} style={{width: 'fit-content', zIndex:'1000', left:'0.5em', top:'0.5em'}}dismissible onClose={() => setShowMessage(false)}>
-                {message}
-                {variant === 'success' && <Check2 />}
-                {variant === 'error' && <XCircleFill /> }
-                {variant === 'warning' && <ExclamationCircleFill />}
-                {variant === 'info' && <InfoCircleFill />}
-            </Alert>
-        </div>
+        <Alert variant={variant} style={{width: 'fit-content', zIndex:'1000', top:'3.5em', right:'0.5em', position:'fixed'}}dismissible onClose={() => setShowMessage(false)}>
+            {message}
+            {variant === 'success' && <Check2 />}
+            {variant === 'error' && <XCircleFill /> }
+            {variant === 'warning' && <ExclamationCircleFill />}
+            {variant === 'info' && <InfoCircleFill />}
+        </Alert>
     );
 }
